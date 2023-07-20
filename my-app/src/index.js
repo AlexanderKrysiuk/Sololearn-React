fa71518ce610b496f3adaf8a63f4f49aab9950a6
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-07-20 17:14:59 +0200

    JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = "David";
+const el = <p>Hello, {name}</p>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+el
 );
 
 // If you want to start measuring performance in your app, pass a function
