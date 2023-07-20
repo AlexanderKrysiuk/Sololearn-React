b787fff636fb1d4310e83e986c4d200ead99f54e
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-07-20 17:35:52 +0200

    Components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,20 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-let counter = 0;
-
-function show() {
-  counter++;
-  const el = <p>{counter}</p> 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-    el
-    );
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello world.</h1>
+  }
 }
 
-setInterval(show, 1000);
+const el = <Hello />;
 
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    el
+  );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
